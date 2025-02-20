35e4bf0f2960e5d57d32ce8f98b8550c73a4649d
Huzaifa <huzefachouhan07@gmail.com>
2025-02-20 15:42:55 +0530

    update the collection section

--- src/main/collection/collectionBig.jsx
+++ src/main/collection/collectionBig.jsx
@@ -4,19 +4,21 @@ import pasta from '../../Images/pasta.png';
 function CollectionBig(props){
     return<>
         <div className='collection-big-row'>
-            <div className='collection-big-img'>
-                <img src={pasta}></img>
-            </div>
-            <div className='collection-big-text'>
-                <h2 className='collection-big-h2'>
-                    {props.bigh2}
-                    Pasta
-                </h2>
-                <p className='collection-big-p'>
-                    {props.bigp}
-                    There are over 600 types of pasta, and some have over 1300 names.<br/>
-                    The three most popular kinds of pasta are penne, spaghetti, and macaroni.
-                </p>
+            <div className='collection-big-curve'>
+                <div className='collection-big-img'>
+                    <img src={pasta}></img>
+                </div>
+                <div className='collection-big-text'>
+                    <h2 className='collection-big-h2'>
+                        {props.bigh2}
+                        Pasta
+                    </h2>
+                    <p className='collection-big-p'>
+                        {props.bigp}
+                        There are over 600 types of pasta, and some have over 1300 names.<br/>
+                        The three most popular kinds of pasta are penne, spaghetti, and macaroni.
+                    </p>
+                </div>
             </div>
         </div>
     </>
